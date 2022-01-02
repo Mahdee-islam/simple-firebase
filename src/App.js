78a71f568e846831a23cb08b410c813336db383d
Mahdi <hirelingmahdi@gmail.com>
2022-01-02 10:59:57 +0600

    firebase authentication

--- src/App.js
+++ src/App.js
@@ -1,25 +1,138 @@
-import logo from './logo.svg';
+import { createUserWithEmailAndPassword, FacebookAuthProvider, getAuth, GoogleAuthProvider, sendEmailVerification, sendPasswordResetEmail, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import { useState } from "react";
 import './App.css';
+import initializeAuthentication from './Firebase/firebse.initialize';
+initializeAuthentication();
+
+const googleProvider = new GoogleAuthProvider();
+const facebookProvider = new FacebookAuthProvider();
+
+
 
 function App() {
+  const auth = getAuth();
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, googleProvider)
+    .then(result => {
+      const user = result.user;
+      console.log(user);
+    })
+  }
+  const handleFacebookSignIn = () => {
+    signInWithPopup(auth, facebookProvider)
+    .then(result => {
+      const user = result.user;
+      console.log(user);
+
+    })
+  }
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [isLogin, setIsLogin] = useState(false);
+
+  const handleRegistration = e => {
+    e.preventDefault();
+    console.log(email, password);
+    if(password.length < 6) {
+      setError('Your password must be at least 6 charecter')
+      return;
+    }
+    if(!/[a-z]/.test(password)) {
+      setError('Your password must contain at least one lower case letter.');
+      return;
+    }
+    if(!/[A-Z]/.test(password)) {
+      setError('Your password must contain at least one upper case letter.');
+      return;
+    }
+
+    isLogin ? processLogin(email,password) : resisterNewUser(email, password)
+   
+   
+  }
+  const emailChange = e => {
+    setEmail(e.target.value);
+  }
+  const passwordChange = e => {
+    setPassword(e.target.value);
+  }
+  const toggleLogin = e => {
+    setIsLogin(e.target.checked);
+  }
+  const resisterNewUser = (email, password) => {
+    createUserWithEmailAndPassword(auth, email, password)
+    .then(result => {
+      const user = result.user;
+      console.log(user);
+      setError('');
+      verifyEmail();
+    })
+    .catch((error) => {
+      setError(error.message)
+    });
+   }
+   const processLogin = (email, password) => {
+    signInWithEmailAndPassword(auth, email, password)
+    .then(result => {
+      const user = result.user;
+      console.log(user);
+      setError('');
+    })
+    .catch(error => {
+      setError(error.message);
+    })
+   }
+   const verifyEmail = () => {
+    sendEmailVerification (auth.currentUser)
+    .then(result => {
+      console.log(result);
+    })
+   }
+   const handleResetPassword = () => {
+    sendPasswordResetEmail(auth, email)
+    .then(result => {
+      
+    })
+   }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="mx-5">
+        <h2 className="text-primary">Please {isLogin ? 'Login' : 'Register' } </h2>
+      <form onSubmit={handleRegistration}>
+  <div className="row mb-3">
+    <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">Email</label>
+    <div className="col-sm-10">
+      <input onBlur={emailChange} type="email" className="form-control" id="inputEmail3"/>
     </div>
-  );
-}
+  </div>
+  <div className="row mb-3">
+    <label htmlFor="inputPassword3" className="col-sm-2 col-form-label">Password</label>
+    <div className="col-sm-10">
+      <input onBlur={passwordChange} type="password" className="form-control" id="inputPassword3"/>
+    </div>
+  </div>
+  <div className="row mb-3">
+    <div className="col-sm-10 offset-sm-2">
+      <div className="form-check">
+        <input onChange={toggleLogin} className="form-check-input" type="checkbox" id="gridCheck1"/>
+        <label className="form-check-label" htmlFor="gridCheck1">
+          Already Registered?
+        </label>
+      </div>
+    </div>
+  </div>
+  <button type="submit" className="btn btn-primary">{isLogin ? 'Login' : "Register"}</button>
+  <button type="button" onClick={handleResetPassword} className="btn btn-secondary">Reset Password</button>
+</form>
+<h3 className="text-danger">{error}</h3>
+      </div>
+      <div>-------------</div>
+      <br /><br /><br />
+      <button onClick={handleGoogleSignIn}>Google Sign in</button>
+      <button onClick={handleFacebookSignIn}>Facebook Signin</button>
+      </div>
+  )}
 
 export default App;
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB4fwmvabXwbpZbOQdxxKEr7lq2e6V6nr8",
+  authDomain: "simple-firebase-auth-bb87f.firebaseapp.com",
+  projectId: "simple-firebase-auth-bb87f",
+  storageBucket: "simple-firebase-auth-bb87f.appspot.com",
+  messagingSenderId: "136241569101",
+  appId: "1:136241569101:web:209195ef9855ecf4b16b04"
+};
+
+export default firebaseConfig;
